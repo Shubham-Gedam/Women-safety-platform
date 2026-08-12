db9dbea9414a661b88a47b689c23c70066bdb69a
Shubham-Gedam <shubhgedam28@gmail.com>
2026-08-12 14:05:11 +0530

    update server listen address to specific IP

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -5,7 +5,7 @@ import { app, socketProxy } from "./src/app.js";
 
 const PORT = process.env.PORT || 8000;
 
-const server = app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, "106.216.242.44/32", () => {
   console.log(`API Gateway running on ${PORT}`);
 });
 
